2dbb42f35d9e64935c1e917f7d5c4de80f5511aa
Lucas Besnard <besnard.lucas@outlook.fr>
2025-03-07 11:52:36 +0100

    Yatzy dojo kata with tests

--- yatzy.test.ts
+++ yatzy.test.ts
@@ -1,7 +1,175 @@
 import { assertEquals } from "@std/assert";
 import { calculateScore } from "./yatzy.ts";
 
-Deno.test("hello", () => {
-  const score = calculateScore([1, 1, 1, 1, 1], "chance");
-  assertEquals(score, 5);
+Deno.test("Testing Yahtzee", async (t) => {
+  await t.step("Yahtzee should return 50", () => {
+    const score = calculateScore([1, 1, 1, 1, 1], "yahtzee");
+    assertEquals(score, 50);
+  });
+
+  await t.step("False Yahtzee should return 0", () => {
+    const score = calculateScore([5, 5, 2, 1, 1], "yahtzee");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Full House", async (t) => {
+  await t.step("Full house should return sum of all dices", () => {
+    const score = calculateScore([5, 5, 5, 1, 1], "full house");
+    assertEquals(score, 17);
+  });
+
+  await t.step(
+    "Full house with pair first should return sum of all dices",
+    () => {
+      const score = calculateScore([5, 5, 1, 1, 1], "full house");
+      assertEquals(score, 13);
+    },
+  );
+
+  await t.step(
+    "Full house with yahtzee should return 0",
+    () => {
+      const score = calculateScore([5, 5, 5, 5, 5], "full house");
+      assertEquals(score, 0);
+    },
+  );
+
+  await t.step("False full house should return 0", () => {
+    const score = calculateScore([5, 2, 3, 0, 1], "full house");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Chance", async (t) => {
+  await t.step("Chance should return sum of all dices", () => {
+    const score = calculateScore([5, 4, 3, 2, 1], "chance");
+    assertEquals(score, 15);
+  });
+});
+
+Deno.test("Testing Large straight", async (t) => {
+  await t.step("Large straight should return sum of all dices", () => {
+    const score = calculateScore([2, 3, 4, 5, 6], "large straight");
+    assertEquals(score, 20);
+  });
+
+  await t.step("False large straight should return 0", () => {
+    const score = calculateScore([2, 3, 3, 5, 6], "large straight");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Small straight", async (t) => {
+  await t.step("Small straight should return sum of all dices", () => {
+    const score = calculateScore([1, 2, 3, 4, 5], "small straight");
+    assertEquals(score, 15);
+  });
+
+  await t.step("False small straight should return 0", () => {
+    const score = calculateScore([1, 2, 3, 3, 5], "small straight");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Four of a kind", async (t) => {
+  await t.step("Four of a kind should return sum of 4 same dices", () => {
+    const score = calculateScore([1, 1, 1, 1, 5], "four of a kind");
+    assertEquals(score, 4);
+  });
+
+  await t.step("False four of a kind should return 0", () => {
+    const score = calculateScore([1, 1, 1, 2, 5], "four of a kind");
+    assertEquals(score, 0);
+  });
+
+  await t.step("False four of a kind with Yahtzee should return 0", () => {
+    const score = calculateScore([1, 1, 1, 1, 1], "four of a kind");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Three of a kind", async (t) => {
+  await t.step("Three of a kind should return sum of 3 same dices", () => {
+    const score = calculateScore([1, 1, 1, 2, 5], "three of a kind");
+    assertEquals(score, 3);
+  });
+
+  await t.step("False three of a kind should return 0", () => {
+    const score = calculateScore([1, 1, 3, 2, 5], "three of a kind");
+    assertEquals(score, 0);
+  });
+
+  await t.step("False three of a kind with Yahtzee should return 0", () => {
+    const score = calculateScore([1, 1, 1, 1, 1], "three of a kind");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Two pairs", async (t) => {
+  await t.step("Two pairs should return sum of the pair dices", () => {
+    const score = calculateScore([1, 1, 2, 3, 3], "two pairs");
+    assertEquals(score, 8);
+  });
+
+  await t.step("False two pairs should return 0", () => {
+    const score = calculateScore([1, 1, 2, 4, 3], "two pairs");
+    assertEquals(score, 0);
+  });
+
+  await t.step("False two pairs should return 0", () => {
+    const score = calculateScore([1, 1, 1, 3, 3], "two pairs");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Pair", async (t) => {
+  await t.step("Pair should return sum of the highest matching dices", () => {
+    const score = calculateScore([1, 1, 4, 3, 3], "pair");
+    assertEquals(score, 6);
+  });
+
+  await t.step("Pair should return sum of the highest matching dices", () => {
+    const score = calculateScore([1, 2, 4, 2, 5], "pair");
+    assertEquals(score, 4);
+  });
+
+  await t.step("False pair should return 0", () => {
+    const score = calculateScore([1, 2, 3, 4, 5], "pair");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Ones", async (t) => {
+  await t.step("One should return sum of all 1 dices", () => {
+    const score = calculateScore([1, 1, 4, 3, 3], "ones");
+    assertEquals(score, 2);
+  });
+
+  await t.step("One should return sum of all 1 dices", () => {
+    const score = calculateScore([1, 1, 4, 1, 3], "ones");
+    assertEquals(score, 3);
+  });
+
+  await t.step("False one should return 0", () => {
+    const score = calculateScore([2, 3, 4, 5, 3], "ones");
+    assertEquals(score, 0);
+  });
+});
+
+Deno.test("Testing Twos", async (t) => {
+  await t.step("Twos should return sum of all 2 dices", () => {
+    const score = calculateScore([2, 2, 4, 3, 3], "twos");
+    assertEquals(score, 4);
+  });
+
+  await t.step("Twos should return sum of all 2 dices", () => {
+    const score = calculateScore([2, 2, 4, 2, 3], "twos");
+    assertEquals(score, 6);
+  });
+
+  await t.step("False twos should return 0", () => {
+    const score = calculateScore([1, 3, 4, 5, 3], "twos");
+    assertEquals(score, 0);
+  });
 });
--- yatzy.ts
+++ yatzy.ts
@@ -1,8 +1,130 @@
 export type Roll = [number, number, number, number, number];
 
-export type Category = string;
+export type Category =
+  | "yahtzee"
+  | "full house"
+  | "chance"
+  | "large straight"
+  | "small straight"
+  | "four of a kind"
+  | "three of a kind"
+  | "two pairs"
+  | "pair"
+  | "ones"
+  | "twos"
+  | "threes"
+  | "fours"
+  | "fives";
+
+function isYahtzee(roll: Roll): boolean {
+  return roll.every((die) => die === roll[0]);
+}
+
+function sumRoll(roll: Roll): number {
+  return roll.reduce((acc, die) => acc + die, 0);
+}
+
+function areEquals(roll: Roll, result: Roll): boolean {
+  return roll.every((die) => result.includes(die));
+}
+
+function getMap(roll: Roll): Map<number, number> {
+  const map = new Map<number, number>();
+  for (const die of roll) {
+    map.set(die, (map.get(die) || 0) + 1);
+  }
+  return map;
+}
+
+function getRepeatedScore(roll: Roll, count: number): number {
+  const map = getMap(roll);
+  for (const [die, dieCount] of map) {
+    if (dieCount === count) {
+      return die * count;
+    }
+  }
+
+  return 0;
+}
+
+function getSumOfDieNumber(roll: Roll, number: number): number {
+  return roll.filter((die) => die === number).reduce(
+    (acc, die) => acc + die,
+    0,
+  );
+}
+
+function getPairs(roll: Roll): [number, number][] {
+  const map = getMap(roll);
+  return [...map.entries()].filter((e) => e[1] === 2);
+}
 
 export function calculateScore(roll: Roll, category: Category): number {
-  console.log(roll, category);
-  return 0;
+  switch (category) {
+    case "yahtzee":
+      return isYahtzee(roll) ? 50 : 0;
+
+    case "chance":
+      return sumRoll(roll);
+
+    case "full house": {
+      const map = getMap(roll);
+      return map.size !== 2 ? 0 : sumRoll(roll);
+    }
+
+    case "large straight": {
+      const result: Roll = [2, 3, 4, 5, 6];
+      const map = getMap(roll);
+      return map.size === 5 && areEquals(roll, result) ? 20 : 0;
+    }
+
+    case "small straight": {
+      const result: Roll = [1, 2, 3, 4, 5];
+      const map = getMap(roll);
+      return map.size === 5 && areEquals(roll, result) ? 15 : 0;
+    }
+
+    case "four of a kind":
+      return getRepeatedScore(roll, 4);
+
+    case "three of a kind":
+      return getRepeatedScore(roll, 3);
+
+    case "two pairs": {
+      const pairs = getPairs(roll);
+      if (pairs.length === 2) {
+        return pairs.reduce((acc, die) => acc + die[0] * die[1], 0);
+      }
+
+      return 0;
+    }
+
+    case "pair": {
+      const pairs = getPairs(roll);
+      if (pairs.length) {
+        pairs.sort((a, b) => b[0] - a[0]);
+        return pairs[0][0] * pairs[0][1];
+      }
+
+      return 0;
+    }
+
+    case "ones":
+      return getSumOfDieNumber(roll, 1);
+
+    case "twos":
+      return getSumOfDieNumber(roll, 2);
+
+    case "threes":
+      return getSumOfDieNumber(roll, 3);
+
+    case "fours":
+      return getSumOfDieNumber(roll, 4);
+
+    case "fives":
+      return getSumOfDieNumber(roll, 5);
+
+    default:
+      return 0;
+  }
 }
